4cab3326a7da79baaa3e76789d2b5c21b63c284a
MicheleCasani <casanimichele@gmail.com>
2025-04-17 16:19:11 +0200

    [DONE] bonus done

--- routers/posts.js
+++ routers/posts.js
@@ -4,29 +4,35 @@ const express = require('express')
 // importo router
 const router = express.Router();
 
+// importo i dati dei post
+const posts = require('../data/data.js')
+
 // index
 router.get('/', (req, res) => {
-    res.send('lista dei post')
+    res.json(posts)
 });
 
 // show
 router.get('/:id', (req, res) => {
-    res.send('visualizzo il singolo post con id:' + req.params.id)
+    const post = posts.find(function (element) {
+        return element.id == req.params.id
+    });
+    res.json(post)
 });
 
 // create
 router.post('/', (req, res) => {
-    res.send('creo un nuovo elemento')
+    res.json('creo un nuovo elemento')
 });
 
 // update
 router.put('/:id', (req, res) => {
-    res.send('mdifico il post con id:' + req.params.id)
+    res.json('mdifico il post con id:' + req.params.id)
 });
 
 // delete
 router.delete('/:id', (req, res) => {
-    res.send('elimino il post con id:' + req.params.id)
+    res.json('elimino il post con id:' + req.params.id)
 });
 
 // esporto il router
