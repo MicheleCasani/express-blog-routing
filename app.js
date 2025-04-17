95bd0b7be4cebe502df1ba29672aa8b1203c807b
MicheleCasani <casanimichele@gmail.com>
2025-04-17 14:21:56 +0200

    [UPD] add folder "public" and img

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const app = express();
 // definisco il numero di porta, ovvero 3000
 const port = 3000;
 
+// importo la cartella public, che contiene file statici
+app.use(express.static('public'));
+
 // definisco la rotta base 
 app.get('/', (req, res)=>{
-    res.send('hello world')
+    res.send('')
 })
 
 // dico al server di rimanere in ascolto sulla porta 3000
