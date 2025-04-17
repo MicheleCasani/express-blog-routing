b9c0eb5cae2f9d7dd93c6e1a8c9bc00d77614166
MicheleCasani <casanimichele@gmail.com>
2025-04-17 14:42:04 +0200

    [UPD[ add route in file posts.js

---
+++ routers/posts.js
@@ -0,0 +1,30 @@
+// importo express
+const express = require('express')
+
+// importo router
+const router = express.Router();
+
+// index
+router.get('/',(req, res)=>{
+    res.send('lista dei post')
+});
+
+// show
+router.get('/:id', (req, res)=>{
+    res.send('visualizzo il singolo post con id:' +  req.params.id)
+});
+
+// create
+router.post('/',(req, res)=>{
+    res.send('creo un nuovo elemento')
+});
+
+// update
+router.put('/:id',(req, res)=>{
+    res.send('mdifico il post con id:' + req.params.id)
+});
+
+// delete
+router.delete('/:id',(req, res)=>{
+    res.send('elimino il post con id:' + req.params.id)
+});
